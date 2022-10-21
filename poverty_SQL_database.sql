cdcdbcbc2aef4725bfa078e1d1f195730b12c998
Damian <damian@borsiak.com>
2022-10-21 15:14:53 +0100

    4 tables uploaded + average column names updated

---
+++ poverty_SQL_database.sql
@@ -0,0 +1,19 @@
+-- create database Proj_Poverty;
+-- use Proj_Poverty;
+
+-- ALTER TABLE gdp_data
+-- RENAME COLUMN Average TO Average_GDP;
+
+-- ALTER TABLE health_data
+-- RENAME COLUMN Average TO Average_health;
+
+-- ALTER TABLE unemploy_data
+-- RENAME COLUMN Average TO Average_unemploy;
+
+-- ALTER TABLE violence
+-- RENAME COLUMN Average TO Average_violence;
+
+
+select * from violence;
+
+
