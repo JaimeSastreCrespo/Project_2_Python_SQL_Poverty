734b8fbf5e6244ec3e13313cb6f0ed6790ccd1f7
Damian <damian@borsiak.com>
2022-10-21 16:45:55 +0100

    Creating internal_poverty using Join

--- poverty_SQL_database.sql
+++ poverty_SQL_database.sql
@@ -1,6 +1,9 @@
 -- create database Proj_Poverty;
 -- use Proj_Poverty;
 
+-- Here we have uploaded 4 databases
+-- After, we have modified 4 Average colum names to unique ones
+
 -- ALTER TABLE gdp_data
 -- RENAME COLUMN Average TO Average_GDP;
 
@@ -13,7 +16,27 @@
 -- ALTER TABLE violence
 -- RENAME COLUMN Average TO Average_violence;
 
+-- TO DO
+-- create empty table for internal poverty index (using join on our 4 tables) = 1 result for each country
+-- create table with exteranal poverty index (upload from internet after modification)
+-- create join table for both results to be compared :) FINITO PEPITO
 
-select * from violence;
+-- INSERT INTO internal_poverty(Country)
+-- select Average_GDP from gdp_data;
+
+-- as (select Average_GDP from gdp_data),
+-- as (select Average_health from health_data),
+-- as (select Average_unemploy from unemploy_data),
+-- as (select Average_violence from violence);
+
+select gd.Country, gd.Average_GDP, hd.Average_health, ud.Average_unemploy, vd.Average_violence
+from gdp_data gd
+left join health_data hd
+on gd.Country = hd.Country
+left join unemploy_data ud
+on hd.Country = ud.Country
+left join violence vd
+on ud.Country = vd.Country;
+
 
 
